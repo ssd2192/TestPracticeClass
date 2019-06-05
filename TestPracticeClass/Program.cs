ed74af9f486bef78588c5fca00928d1f5e9e7dae
Sandeep <ssd2192@gmail.com>
2019-06-05 08:29:37 -0400

    Palindrom Program

---
+++ TestPracticeClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TestPracticeClass
+{
+    class Program
+    {
+        static string str;
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter the Number: ");
+            str = Console.ReadLine();
+
+            if (IsPalindrom(str))
+            {
+                Console.WriteLine("Palindrom");
+            }
+            else
+                Console.WriteLine("not palindrom");
+
+            Console.ReadKey();
+        }
+
+        public static bool IsPalindrom(string str)
+        {
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                if (str[i] != str[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
